412d8ede63b1f7a048cc244aa40bb96fe2ba80b9
danaxa98 <danbing123@gmail.com>
2019-01-03 11:06:14 -0500

    Version 0.0.5
    Corrected some typos, encapsulated most of the protected variables and created helper methods. Cleaned some codes

--- src/Grid.java
+++ src/Grid.java
@@ -3,9 +3,9 @@ import util.Errors;
 public class Grid {
 	
 	private final static int DEFAULT_GRID_SIZE = 60;
-	protected int sizeX;
-	protected int sizeY;
-	Tile[][] gridMap;
+	private final int sizeX;
+	private final int sizeY;
+	private Tile[][] gridMap;
 	
 	/**
 	 * initializes an empty grid of square size 60
@@ -25,15 +25,12 @@ public class Grid {
 		if(x <= 0) {		//Checks for invalid input
 			System.err.println(Errors.INVALID_GRID_SIZE.toString());
 		}
-		gridMap = new Tile[x][x];
-		for(int i = 0; i < gridMap.length; i++) {
-			for(int j = 0; j < gridMap[0].length; j++) {
-				Tile t = new Tile(i, j);	//Create new empty tile with coordinate informations
-				gridMap[i][j] = t;
-			}
-		}
+		sizeX = x;
+		sizeY = x;
+		drawGridMap(sizeX, sizeY);
 	}
 	
+
 	/**
 	 * @param size x
 	 * @param size y
@@ -44,6 +41,18 @@ public class Grid {
 		if(x <= 0 || y <= 0) {		//Checks for invalid input
 			System.err.println(Errors.INVALID_GRID_SIZE.toString());
 		}
+		sizeX = x;
+		sizeY = y;
+		gridMap = new Tile[x][y];
+		drawGridMap(sizeX, sizeY);
+	}
+	
+	/**
+	 * @param int x
+	 * @param int y
+	 * Create an empty grid of default tiles with dimensions x,y
+	 */
+	private void drawGridMap(int x, int y) {
 		gridMap = new Tile[x][y];
 		for(int i = 0; i < gridMap.length; i++) {
 			for(int j = 0; j < gridMap[0].length; j++) {
@@ -51,6 +60,27 @@ public class Grid {
 				gridMap[i][j] = t;
 			}
 		}
+	}
+
+	/**
+	 * @return the sizeX
+	 */
+	public int getX() {
+		return sizeX;
+	}
+
+	/**
+	 * @return the sizeY
+	 */
+	public int getY() {
+		return sizeY;
+	}
+	
+	/**
+	 * @return gridMap
+	 */
+	public Tile[][] getGridMap() {
+		return gridMap;
 	}
 	
 }
--- src/School.java
+++ src/School.java
@@ -1,24 +1,136 @@
 
 public abstract class School {
 	Grid schoolGrid;
-	int numStudent;
-	int numTeacher;
-	int numJanitor;
-	int numChef;
-	int numSecurity;
-	int numDepartment;
-	int numClass;
+	private int numStudent;
+	private int numTeacher;
+	private int numJanitor;
+	private int numChef;
+	private int numSecurity;
+	private int numDepartment;
+	private int numClass;
 	// double wealth;
 	// double reputation;
 	
 	
 	public School() {
-		numStudent = 0;
-		numTeacher = 0;
-		numJanitor = 0;
-		numChef = 0;
-		numSecurity = 0;
-		numDepartment = 0;
-		numClass = 0;
+		setNumStudent(0);
+		setNumTeacher(0);
+		setNumJanitor(0);
+		setNumChef(0);
+		setNumSecurity(0);
+		setNumDepartment(0);
+		setNumClass(0);
+	}
+
+
+	/**
+	 * @return the numStudent
+	 */
+	public int getNumStudent() {
+		return numStudent;
+	}
+
+
+	/**
+	 * @param numStudent the numStudent to set
+	 */
+	public void setNumStudent(int numStudent) {
+		this.numStudent = numStudent;
+	}
+
+
+	/**
+	 * @return the numTeacher
+	 */
+	public int getNumTeacher() {
+		return numTeacher;
+	}
+
+
+	/**
+	 * @param numTeacher the numTeacher to set
+	 */
+	public void setNumTeacher(int numTeacher) {
+		this.numTeacher = numTeacher;
+	}
+
+
+	/**
+	 * @return the numJanitor
+	 */
+	public int getNumJanitor() {
+		return numJanitor;
+	}
+
+
+	/**
+	 * @param numJanitor the numJanitor to set
+	 */
+	public void setNumJanitor(int numJanitor) {
+		this.numJanitor = numJanitor;
+	}
+
+
+	/**
+	 * @return the numChef
+	 */
+	public int getNumChef() {
+		return numChef;
+	}
+
+
+	/**
+	 * @param numChef the numChef to set
+	 */
+	public void setNumChef(int numChef) {
+		this.numChef = numChef;
+	}
+
+
+	/**
+	 * @return the numDepartment
+	 */
+	public int getNumDepartment() {
+		return numDepartment;
+	}
+
+
+	/**
+	 * @param numDepartment the numDepartment to set
+	 */
+	public void setNumDepartment(int numDepartment) {
+		this.numDepartment = numDepartment;
+	}
+
+
+	/**
+	 * @return the numSecurity
+	 */
+	public int getNumSecurity() {
+		return numSecurity;
+	}
+
+
+	/**
+	 * @param numSecurity the numSecurity to set
+	 */
+	public void setNumSecurity(int numSecurity) {
+		this.numSecurity = numSecurity;
+	}
+
+
+	/**
+	 * @return the numClass
+	 */
+	public int getNumClass() {
+		return numClass;
+	}
+
+
+	/**
+	 * @param numClass the numClass to set
+	 */
+	public void setNumClass(int numClass) {
+		this.numClass = numClass;
 	} 
 }
